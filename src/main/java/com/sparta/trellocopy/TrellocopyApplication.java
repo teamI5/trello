62ca4577fcbce1a00e030f1598bf063651ce4b63
20240729 <yoohyeonsik@gmail.com>
2024-10-14 13:44:46 +0900

    first commit

---
+++ src/main/java/com/sparta/trellocopy/TrellocopyApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.trellocopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrellocopyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrellocopyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/trellocopy/TrellocopyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.trellocopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrellocopyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
