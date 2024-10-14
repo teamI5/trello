d2f07a194b30870162977475545df08a0705d9d8
20240729 <yoohyeonsik@gmail.com>
2024-10-14 16:03:54 +0900

    add : entity

--- src/main/java/com/sparta/trellocopy/domain/workspace/entity/WorkSpace.java
+++ src/main/java/com/sparta/trellocopy/domain/workspace/entity/WorkSpace.java
@@ -1,13 +1,25 @@
 package com.sparta.trellocopy.domain.workspace.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.sparta.trellocopy.domain.board.entity.Board;
+import com.sparta.trellocopy.domain.common.entity.Timestamped;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
-public class WorkSpace {
+@Getter
+@NoArgsConstructor
+public class WorkSpace extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
+
+    private String description;
+
+    @OneToMany(mappedBy = "workspace", cascade = CascadeType.ALL)
+    private List<Board> boards;
 }
