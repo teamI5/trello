8a2fe18e3bc6e6a4cd449a84d79a55d6e1f84f67
EtherXion <etel0103@gmail.com>
2024-10-14 16:41:29 +0900

    add : comment 기본 틀

--- src/main/java/com/sparta/trellocopy/domain/comment/Dto/CommentResponseDto.java
+++ src/main/java/com/sparta/trellocopy/domain/comment/Dto/CommentResponseDto.java
@@ -7,6 +7,8 @@ public class CommentResponseDto {
     private Long id;
     private String content;
 
+
+
     public CommentResponseDto(Long id, String content) {
         this.id = id;
         this.content = content;
--- src/main/java/com/sparta/trellocopy/domain/comment/Service/CommentService.java
+++ src/main/java/com/sparta/trellocopy/domain/comment/Service/CommentService.java
@@ -1,4 +1,41 @@
 package com.sparta.trellocopy.domain.comment.Service;
 
+import com.sparta.trellocopy.domain.card.entity.Card;
+import com.sparta.trellocopy.domain.card.repository.CardRepository;
+import com.sparta.trellocopy.domain.comment.Dto.CommentRequestDto;
+import com.sparta.trellocopy.domain.comment.Dto.CommentResponseDto;
+import com.sparta.trellocopy.domain.comment.entity.Comment;
+import com.sparta.trellocopy.domain.comment.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.security.InvalidParameterException;
+
+@Service
+@RequiredArgsConstructor
 public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CardRepository cardRepository;
+
+    public CommentResponseDto addComment(long cardId, CommentRequestDto commentRequestDto) {
+
+        Card card = cardRepository.findById(cardId)
+                .orElseThrow(() -> new InvalidParameterException("해당 카드가 존재하지 않음"));
+
+        Comment comment = new Comment(
+            commentRequestDto.getContent(),
+            user,
+            card
+        );
+
+        Comment savedComment = commentRepository.save(comment);
+
+        return new CommentResponseDto(
+            savedComment.getContent()
+        )
+
+    }
+
 }
