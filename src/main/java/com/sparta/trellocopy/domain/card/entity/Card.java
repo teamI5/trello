c3d536bd542d608a3914c33d215eb7c228f59fe1
Hyemin Yang <asitwas729@gmail.com>
2024-10-14 16:25:28 +0900

    add : Card Entity 세팅

--- src/main/java/com/sparta/trellocopy/domain/card/entity/Card.java
+++ src/main/java/com/sparta/trellocopy/domain/card/entity/Card.java
@@ -1,13 +1,31 @@
 package com.sparta.trellocopy.domain.card.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.sparta.trellocopy.domain.common.entity.Timestamped;
+import com.sparta.trellocopy.domain.list.entity.List;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 @Entity
-public class Card {
+@NoArgsConstructor
+public class Card extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String title;
+
+    @Column(length = 100)
+    private String contents;
+
+    private LocalDateTime deadline;
+
+    @OneToMany(mappedBy = "card")
+    private ArrayList<List> list;
+
+    private String file_url;
+
 }
