7cd8d135b05834c43aadf48a01c522bdac19f52f
20240729 <yoohyeonsik@gmail.com>
2024-10-14 16:25:05 +0900

    add :service, controller 작업중

--- src/main/java/com/sparta/trellocopy/domain/workspace/controller/WorkSpaceController.java
+++ src/main/java/com/sparta/trellocopy/domain/workspace/controller/WorkSpaceController.java
@@ -1,7 +1,12 @@
 package com.sparta.trellocopy.domain.workspace.controller;
 
+import com.sparta.trellocopy.domain.workspace.dto.WorkSpaceRequest;
+import com.sparta.trellocopy.domain.workspace.dto.WorkSpaceResponse;
 import com.sparta.trellocopy.domain.workspace.service.WorkSpaceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class WorkSpaceController {
 
     private final WorkSpaceService workSpaceService;
+
+    @PostMapping("/workspaces")
+    public ResponseEntity<WorkSpaceResponse> saveWorkSpace(@RequestBody WorkSpaceRequest workSpaceRequest) {
+        return ResponseEntity.ok(workSpaceService)
+    }
 }
--- src/main/java/com/sparta/trellocopy/domain/workspace/service/WorkSpaceService.java
+++ src/main/java/com/sparta/trellocopy/domain/workspace/service/WorkSpaceService.java
@@ -1,5 +1,7 @@
 package com.sparta.trellocopy.domain.workspace.service;
 
+import com.sparta.trellocopy.domain.workspace.dto.WorkSpaceRequest;
+import com.sparta.trellocopy.domain.workspace.dto.WorkSpaceResponse;
 import com.sparta.trellocopy.domain.workspace.repository.WorkSpaceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,4 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class WorkSpaceService {
 
     private final WorkSpaceRepository workSpaceRepository;
+
+    @Transactional
+    public WorkSpaceResponse saveWorkSpace(WorkSpaceRequest workSpaceRequest) {}
 }
