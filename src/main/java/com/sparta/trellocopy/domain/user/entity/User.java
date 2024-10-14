947c98bca51ee1b6ed35a3effb9e77c3628f9dd6
golden-hamster <isack19999@gmail.com>
2024-10-14 16:10:44 +0900

    feat: 사용자 인증/인가 구현

---
+++ src/main/java/com/sparta/trellocopy/domain/user/entity/User.java
@@ -0,0 +1,35 @@
+package com.sparta.trellocopy.domain.user.entity;
+
+import com.sparta.trellocopy.domain.common.entity.Timestamped;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class User extends Timestamped {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private UserRole role;
+
+    private Boolean deleted;
+
+    @Builder
+    private User(String email, String password, UserRole role, Boolean deleted) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.deleted = deleted;
+    }
+}
---
+++ src/main/java/com/sparta/trellocopy/domain/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.sparta.trellocopy.domain.user.repository;
+
+import com.sparta.trellocopy.domain.user.entity.User;
+import com.sparta.trellocopy.domain.user.exception.UserNotFoundException;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    default User findByIdOrElseThrow(Long userId) {
+        return findById(userId).orElseThrow(UserNotFoundException::new);
+    }
+}
