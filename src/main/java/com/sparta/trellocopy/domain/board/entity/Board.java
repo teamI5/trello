e5fabe36ad811bad2114267fc52b7c46f149b67d
20240729 <yoohyeonsik@gmail.com>
2024-10-14 15:06:10 +0900

    보드 엔티티

---
+++ src/main/java/com/sparta/trellocopy/domain/board/entity/Board.java
@@ -0,0 +1,12 @@
+package com.sparta.trellocopy.domain.board.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Board {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
