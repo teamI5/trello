7f31616452a6a2ded33a00eaabce0338911f5c65
Hyemin Yang <asitwas729@gmail.com>
2024-10-15 17:06:23 +0900

    add : AuthUser 생성

---
+++ src/main/java/com/sparta/trellocopy/domain/common/dto/AuthUser.java
@@ -0,0 +1,25 @@
+package com.sparta.trellocopy.domain.common.dto;
+
+import com.sparta.trellocopy.domain.user.entity.UserRole;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+public class AuthUser {
+
+    private final Long id;
+    private final String email;
+    private final String nickname;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public AuthUser(Long id, String email, String nickname, UserRole userRole) {
+        this.id = id;
+        this.email = email;
+        this.nickname = nickname;
+        this.authorities = List.of(new SimpleGrantedAuthority(userRole.name()));
+    }
+}
