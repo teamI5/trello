381702b54e7271413707b4ccd529b71969f13b5a
golden-hamster <isack19999@gmail.com>
2024-10-15 10:52:12 +0900

    add: 연관관계 엔티티 관련 구현

---
+++ src/main/java/com/sparta/trellocopy/domain/user/entity/WorkspaceUser.java
@@ -0,0 +1,37 @@
+package com.sparta.trellocopy.domain.user.entity;
+
+import com.sparta.trellocopy.domain.workspace.entity.WorkSpace;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class WorkspaceUser {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private WorkSpace workspace;
+
+    @Enumerated(EnumType.STRING)
+    private WorkspaceRole role;
+
+    @Builder
+    private WorkspaceUser(User user, WorkSpace workspace, WorkspaceRole role) {
+        this.user = user;
+        this.workspace = workspace;
+        this.role = role;
+    }
+
+    public void updateRole(WorkspaceRole role) {
+        this.role = role;
+    }
+}
