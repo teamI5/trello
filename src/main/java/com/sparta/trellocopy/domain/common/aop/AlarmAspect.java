c4daf8fd41379da090991cbfc86f1fbe09637e11
20240729 <yoohyeonsik@gmail.com>
2024-10-16 19:45:26 +0900

    fix : 알림 설정 aop의 URL을 환경 변수를 통해 설정하도록 변경

--- src/main/java/com/sparta/trellocopy/domain/common/aop/AlarmAspect.java
+++ src/main/java/com/sparta/trellocopy/domain/common/aop/AlarmAspect.java
@@ -3,6 +3,7 @@ package com.sparta.trellocopy.domain.common.aop;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -13,13 +14,15 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class AlarmAspect {
 
+    // 알림 보낼 url
+    @Value("${WEBHOOKS_URL}")
+    String url;
+
     // 일단 알림 요청 보내는 메서드
     @Before("@annotation(Alarm)")
     public void sendAlarm(JoinPoint joinPoint){
-        RestTemplate restTemplate = new RestTemplate();
 
-        // 알림 보낼 url
-        String url = "https://hooks.slack.com/services/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";
+        RestTemplate restTemplate = new RestTemplate();
 
         // 실행될 메서드의 이름을 가져옴
         String text = joinPoint.getSignature().getName();
