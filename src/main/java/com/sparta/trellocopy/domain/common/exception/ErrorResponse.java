32862e2ca56aabff96cb2d1d8c53ce54428a3f7a
golden-hamster <isack19999@gmail.com>
2024-10-18 04:28:31 +0900

    add: 예외 처리 구현

---
+++ src/main/java/com/sparta/trellocopy/domain/common/exception/ErrorResponse.java
@@ -0,0 +1,10 @@
+package com.sparta.trellocopy.domain.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ErrorResponse {
+    private String message;
+}
