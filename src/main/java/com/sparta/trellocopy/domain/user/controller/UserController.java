f0395b0d11656e3e609a781ae03fdca4ac7071cf
golden-hamster <isack19999@gmail.com>
2024-10-18 04:18:14 +0900

    refactor: url 컨벤션 맞추기

--- src/main/java/com/sparta/trellocopy/domain/user/controller/UserController.java
+++ src/main/java/com/sparta/trellocopy/domain/user/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/users")
+@RequestMapping("/users")
 @RestController
 public class UserController {
 
